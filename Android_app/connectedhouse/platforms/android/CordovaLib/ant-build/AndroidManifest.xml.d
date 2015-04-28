6660d16d31d3617f272c2f7e5a8ba017fec5c436
albertcalv <albertcalv@gmail.com>
2015-04-29 00:00:40 +0200

    Android app

---
+++ Android_app/connectedhouse/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/montse/Projects/CONNECTEDHOUSE/Android_app/connectedhouse/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/montse/Projects/CONNECTEDHOUSE/Android_app/connectedhouse/platforms/android/CordovaLib/AndroidManifest.xml \
