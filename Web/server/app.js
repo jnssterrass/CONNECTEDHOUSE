296b76c43e1366b3928835bcc68cfc965c733d91
albertcalv <albertcalv@gmail.com>
2015-05-03 11:39:12 +0200

    new web base project

--- Web/server/app.js
+++ Web/server/app.js
@@ -7,18 +7,30 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var express = require('express');
+var express  = require('express');
 var mongoose = require('mongoose');
-var config = require('./config/environment');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var config   = require('./config/environment');
 
 // Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options);
+//mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connect(configDB.url); // connect to our database
 
 // Populate DB with sample data
 if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
+    app.use(morgan('dev'));
+    app.use(cookieParser());
+    app.use(bodyParser());
+    app.set('view engine', 'ejs');
+    app.use(session({ secret: 'albertjuandidacjonathan' }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(flash());
+
 var server = require('http').createServer(app);
 var socketio = require('socket.io')(server, {
   serveClient: (config.env === 'production') ? false : true,
@@ -26,7 +38,8 @@ var socketio = require('socket.io')(server, {
 });
 require('./config/socketio')(socketio);
 require('./config/express')(app);
-require('./routes')(app);
+
+require('./routes')(app, passport);
 
 // Start server
 server.listen(config.port, config.ip, function () {
