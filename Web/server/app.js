72d89ea592412485d1c446198cb5dfe6cd2700f8
albertcalv <albertcalv@gmail.com>
2015-05-04 16:27:30 +0200

    Added Raspberry Management on app.js

--- Web/server/app.js
+++ Web/server/app.js
@@ -7,9 +7,13 @@
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var express = require('express');
+var express = require('express'),
+      bodyParser = require('body-parser'),
+      request = require('request');
+
 var mongoose = require('mongoose');
 var config = require('./config/environment');
+var multer = require('multer');
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
@@ -19,6 +23,8 @@ if(config.seedDB) { require('./config/seed'); }
 
 // Setup server
 var app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
 var server = require('http').createServer(app);
 var socketio = require('socket.io')(server, {
   serveClient: (config.env === 'production') ? false : true,
@@ -27,6 +33,32 @@ var socketio = require('socket.io')(server, {
 require('./config/socketio')(socketio);
 require('./config/express')(app);
 require('./routes')(app);
+
+app.post('/forwardtoarduino', function (req, res) {
+	var options = {
+        url: '192.168.1.177',
+        form: {
+		    "device": req.body.device,
+		    "action": req.body.action,
+		    "value": req.body.value,
+		    "response": ""
+		}
+	};
+
+	request.post(options, function (err, httpResponse, body) {
+
+            if (!err && httpResponse.statusCode == 200) {
+
+                console.log('success');
+                res.send('ha ido bien');
+
+            } else {
+
+            	console.log('pinch');
+            	res.send('hemos pinchado');
+            }
+        });
+});
 
 // Start server
 server.listen(config.port, config.ip, function () {
