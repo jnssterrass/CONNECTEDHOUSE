d994853b414b8b43c474852e42f8c7431219d807
albertcalv <albertcalv@gmail.com>
2015-05-06 01:10:30 +0200

    Toda la vista integrada

--- Web/client/app/portada/portada.controller.js
+++ Web/client/app/portada/portada.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('connectedHouseApp', [])
-  .controller('PortadaCtrl', function ($scope, $http, socket)  {
-    $scope.message = 'Hello';
+  .controller('PortadaCtrl', function ($scope)  {
+    $scope.message = 'Turbomax 3000';
     $scope.hola = 'angular funciona';
   });
