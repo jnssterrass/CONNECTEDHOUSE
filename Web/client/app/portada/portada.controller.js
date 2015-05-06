c7e32ed9215521524176dbe56b4aaf982dc4fbc3
albertcalv <albertcalv@gmail.com>
2015-05-07 00:03:25 +0200

    View Error

--- Web/client/app/portada/portada.controller.js
+++ Web/client/app/portada/portada.controller.js
@@ -5,15 +5,33 @@ angular.module('connectedHouseApp', [])
   .controller('PortadaCtrl', function ($scope)  {
 
     $scope.devices = [];
-
-    //Populate devices
+    //Populate devices -> aqui en vez de llenar el array con datos
+    //locales hay que leer de la BD y todos los ID's que hay en el
+    //sistema y guardarlos en devices
     $scope.devices.push("34");
     $scope.devices.push("51");
     $scope.devices.push("8");
     $scope.devices.push("7");
 
-    
 
+
+    $scope.device_info = function(device) {
+      if(device =="34") {
+        $info.name = "Lavadora";
+      }else {
+        $info.name = "Horno";
+      }
+
+    };
+
+
+  /*  $scope.$watch("deviceid",function(newValue, oldValue)) {
+        if(newValue, oldValue) {
+          return;
+        }
+        $scope.name="Lavadora";
+    });
+*/
 
 
   });
