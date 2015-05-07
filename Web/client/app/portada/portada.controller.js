d5e051ad08840867d3cb89baf06b5cf1d3e941ee
albertcalv <albertcalv@gmail.com>
2015-05-08 01:07:11 +0200

    module connection mongolab

--- Web/client/app/portada/portada.controller.js
+++ Web/client/app/portada/portada.controller.js
@@ -1,58 +1,22 @@
 'use strict';
 
-
-angular.module('resource', ['ngResource'])
-  .factory('devices', function ($resource) {
-    var devices = $resource('  https://api.mongolab.com/api/1/databases/ch-repo/collections/devices:id',
-    {
-      apiKey:'2UkXrp3c_Kk9rJgB3PBfNL1zH2lg_xSd',
-      id:'@_id.$oid'
-    });
-    return devices;
-});
-
-
-angular.module('resource1', ['ngResource'])
-  .factory('devices', function ($resource) {
-    var devices = $resource('https://api.mongolab.com/api/1/databases/ch-repo/collections/status:id',
-    {
-      apiKey:'2UkXrp3c_Kk9rJgB3PBfNL1zH2lg_xSd',
-      id:'@_id.$oid'
-    });
-    return status;
-});
-
 angular.module('connectedHouseApp', [])
-  .controller('PortadaCtrl', function ($scope, $http)  {
+  /*.factory('Devices_info', function ($resource) {
+    var Devices_info = $resource('https://api.mongolab.com/api/1/databases/ch-repo/collections/devices:id',
+    {
+      apiKey:'2UkXrp3c_Kk9rJgB3PBfNL1zH2lg_xSd',
+      id:'@_id.$oid'
+    });
+    return Devices_info;
+});*/
 
-    $scope.devices = [];
-    //Populate devices -> aqui en vez de llenar el array con datos
-    //locales hay que leer de la BD y todos los ID's que hay en el
-    //sistema y guardarlos en devices
-    $scope.devices.push("34");
-    $scope.devices.push("51");
-    $scope.devices.push("8");
-    $scope.devices.push("7");
-
-
-
-
-
-
-
-    /*
-    $http.post('/api/repo/', { name: ALBERT });
-    */
-
-
-    /*
-    https://api.mongolab.com/api/1/databases/ch-repo/
-        collections/devices?apiKey=2UkXrp3c_Kk9rJgB3PBfNL1zH2lg_xSd
-    */
-
-
-
-
+  .controller('PortadaCtrl', function ($scope, $http /*,Devices_info*/)  {
+    /*  $scope.Devices_info = Devices_info.query();*/
+      $scope.devices = [];
+      $scope.devices.push("34");
+      $scope.devices.push("51");
+      $scope.devices.push("8");
+      $scope.devices.push("7");
 
     $scope.device_info = function(device) {
       if(device =="34") {
@@ -63,4 +27,4 @@ angular.module('connectedHouseApp', [])
 
     };
 
-  });
+});
