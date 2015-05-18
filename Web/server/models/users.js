40083b7f74621f08429b3712b21fd14230e732d9
Jona <jona@localhost.localdomain>
2015-05-18 02:11:51 +0200

    signup funciona y login

--- Web/server/models/users.js
+++ Web/server/models/users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
+var bcrypt   = require('bcrypt-nodejs')
 
 var usersSchema = new Schema({
   user     :{ type: String },
@@ -7,4 +8,14 @@ var usersSchema = new Schema({
   mode     :{ type: String }
 });
 
+usersSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+
+usersSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
 module.exports = mongoose.model('users', usersSchema);
+
