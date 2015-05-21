fd65c24ae34e064f9a7c291a1320453dd76454bb
Juan Salmerón Moya <jnssterrass@gmail.com>
2015-05-22 00:19:46 +0200

    añadidos los dos arduinos (falta actualizar el deviceID del ikealamp) y un template como referencia para hacer otros.

--- Arduino/ikeaLamp/ikeaLamp.ino
+++ Arduino/ikeaLamp/ikeaLamp.ino
@@ -19,7 +19,7 @@
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-IPAddress ip(192, 168, 137, 6);
+IPAddress ip(192, 168, 137, 5);
 //IPAddress ip(169, 254, 122, 85);
 
 //Relay 1 attached to pin(2)
